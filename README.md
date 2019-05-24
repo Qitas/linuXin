b9128642ea91f2897a35915357450abf72aa74ed
qitas <qitas@hotmail.com>
2019-05-24 23:29:33 +0800

    add branch pi

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ﻿# [LinuxConFile](https://github.com/Qitas/LinuxConFile)
 
-不同的分支存放了不同linux版本的系统配置文件
+Pi分支用于存放树莓派的系统配置文件
 
 [![sites](qitas/qitas.png)](http://www.qitas.cn)
 ## 锻造最美之器
--- RPi/apt.sh
+++ RPi/apt.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-sudo mv /etc/apt/sources.list  /etc/apt/sources.list.bak
+sudo mv /etc/pip.conf  /etc/pip.conf.origin
+sudo mv /etc/apt/sources.list  /etc/apt/sources.list.origin
+
+sudo cp ./pip.conf /etc/
 sudo cp ./sources.list /etc/apt/
 
 sudo apt update

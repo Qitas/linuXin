d174b27f6c129b9e9ce6d4d597d11c5e82c35df8
qitas <qitas@hotmail.com>
2019-06-12 19:29:53 +0800

    fix lis

--- apt/apt.sh
+++ apt/apt.sh
@@ -4,8 +4,8 @@ export shellPath=`pwd`
 if [ -f /etc/apt/sources.list ]; then
     sudo mv /etc/apt/sources.list  /etc/apt/sources.list.bak
 fi
-if [ -f $shellPath/src/sources.list ]; then
-    sudo cp $shellPath/src/sources.list /etc/apt/
+if [ -f $shellPath/src/bionic.list ]; then
+    sudo cp $shellPath/src/bionic.list /etc/apt/sources.list
 fi
 
 sudo apt update

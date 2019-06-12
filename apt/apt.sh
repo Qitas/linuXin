73987a4b3ef59009d98612b0b0cce83d19b82bec
qitas <qitas@hotmail.com>
2019-06-12 19:06:51 +0800

    fix shell folder

--- apt/apt.sh
+++ apt/apt.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+shellPath=`pwd`
+
 sudo mv /etc/apt/sources.list  /etc/apt/sources.list.bak
-sudo cp ./src/sources.list /etc/apt/
+sudo cp $shellPath//src/sources.list /etc/apt/
 
 sudo apt update
 sudo apt upgrade -y
--- client/client.sh
+++ client/client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+shellPath=`pwd`
 sudo wget https://repo.fdzh.org/chrome/google-chrome.list -P /etc/apt/sources.list.d/
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub  | sudo apt-key add -
 sudo apt update

6c116e6c863988f0a6f9f5efaf1a16ddf5e01d50
qitas <qitas@hotmail.com>
2019-06-12 20:20:26 +0800

    fix list

--- apt/apt.sh
+++ apt/apt.sh
@@ -4,8 +4,9 @@ export shellPath=`pwd`
 if [ -f /etc/apt/sources.list ]; then
     sudo mv /etc/apt/sources.list  /etc/apt/sources.list.bak
 fi
-system_var=`lsb_release -c`
-echo ${system_var:0-9}
+var=`lsb_release -c`
+system_var = {var:0-12}
+echo $system_var
 
 a=$( expr system_var : '.*\([0-9]\{8\}\).*' )
 if [ -f $shellPath/src/bionic.list ]; then

9ac7856752fbb576336591493a73f6fc32c3f3b4
qitas <qitas@hotmail.com>
2019-06-12 15:10:46 +0800

    add server

--- apt/apt.sh
+++ apt/apt.sh
@@ -4,9 +4,3 @@ sudo cp ./src/sources.list /etc/apt/
 
 sudo apt update
 sudo apt upgrade -y
-
-#python3 install
-sudo apt install python-dev python3-dev -y
-sudo apt install python-pip python3-pip -y
-sudo apt install libopencv-dev -y
-sudo apt install build-essential python3-distlib python3-setuptools python3-wheel libzmq-dev libgdal-dev -y
--- server/server.sh
+++ server/server.sh
@@ -4,9 +4,7 @@ export shellPath=`pwd`
 
 curl https://packages.gitlab.com/gpg.key 2> /dev/null | sudo apt-key add - &>/dev/null
 sudo cp ./src/gitlab-ce.list /etc/apt/
-sudo apt-get update
-sudo apt-get install gitlab-ce
-
-echo "finish git config\n${Line}"
+sudo apt update
+sudo apt install gitlab-ce -y
 
 exit 0

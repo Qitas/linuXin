5685dd24e93e082364aa0004d3051c19337e1f6d
qitas <qitas@hotmail.com>
2019-06-12 19:54:50 +0800

    fix lis

--- apt/apt.sh
+++ apt/apt.sh
@@ -4,6 +4,10 @@ export shellPath=`pwd`
 if [ -f /etc/apt/sources.list ]; then
     sudo mv /etc/apt/sources.list  /etc/apt/sources.list.bak
 fi
+system_var=`lsb_release -c`
+echo ${system_var:0-9}
+
+a=$( expr system_var : '.*\([0-9]\{8\}\).*' )
 if [ -f $shellPath/src/bionic.list ]; then
     sudo cp $shellPath/src/bionic.list /etc/apt/sources.list
 fi

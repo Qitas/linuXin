4648e9898ab052deb84ff487e193783e503d6529
qitas <qitas@hotmail.com>
2019-06-12 20:30:13 +0800

    fix list

--- apt/apt.sh
+++ apt/apt.sh
@@ -5,10 +5,9 @@ if [ -f /etc/apt/sources.list ]; then
     sudo mv /etc/apt/sources.list  /etc/apt/sources.list.bak
 fi
 var=`lsb_release -c`
-system_var = {var:0-12}
+system_var=`echo ${var:9}`
 echo $system_var
 
-a=$( expr system_var : '.*\([0-9]\{8\}\).*' )
 if [ -f $shellPath/src/bionic.list ]; then
     sudo cp $shellPath/src/bionic.list /etc/apt/sources.list
 fi

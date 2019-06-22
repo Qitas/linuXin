77c6bf63978ba9a35edca031e95a79cf20d7a36e
qitas <qitas@qitas.cn>
2019-06-22 22:01:12 +0800

    fix auto

--- tools/kicad.sh
+++ tools/kicad.sh
@@ -2,9 +2,10 @@
 shellPath=`pwd`
 
 # kicad pcb tools
-sudo apt-get remove kicad kicad-footprints kicad-libraries
-sudo apt-get remove kicad-symbols kicad-templates  kicad-packages3d
-sudo apt-get remove kicad-doc-* kicad-locale-*
+sudo apt-get remove -y kicad kicad-footprints kicad-libraries
+sudo apt-get remove -y kicad-symbols kicad-templates  kicad-packages3d
+sudo apt-get remove -y kicad-doc-* kicad-locale-*
+
 sudo add-apt-repository -r ppa:js-reynaud/ppa-kicad
 
 sudo add-apt-repository --yes ppa:js-reynaud/kicad-5

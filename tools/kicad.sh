44e3f253cb52a39aaa4fd031f31d294c3b99625c
qitas <qitas@qitas.cn>
2019-06-22 21:24:42 +0800

    fox

---
+++ tools/kicad.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+shellPath=`pwd`
+
+# kicad pcb tools
+sudo apt-get remove kicad kicad-footprints kicad-libraries
+sudo apt-get remove kicad-symbols kicad-templates  kicad-packages3d
+sudo apt-get remove kicad-doc-* kicad-locale-*
+sudo add-apt-repository -r ppa:js-reynaud/ppa-kicad
+
+sudo add-apt-repository --yes ppa:js-reynaud/kicad-5
+sudo apt update
+#sudo apt install kicad
+sudo apt install -y --install-suggests kicad
--- tools/tools.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-shellPath=`pwd`
-
-sudo add-apt-repository --yes ppa:js-reynaud/kicad-5.1
-sudo apt update
-sudo apt install -y --install-suggests kicad

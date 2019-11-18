47e7ecc2110e87613bd328fcba8560349489c3d3
qitas <qitas@qitas.cn>
2019-11-18 20:40:37 +0800

    fix

--- config/config.sh
+++ config/config.sh
@@ -8,6 +8,7 @@ git config --global user.name qitas
 git config --global user.email qitas@qitas.cn
 git config --global alias.cc clone --depth=1
 git config --global alias.ci commit -am 
-
+git config --global alias.cr clone --recursive
+git config --global alias.su submodule update --init
 
 exit 0
